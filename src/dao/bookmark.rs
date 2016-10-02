184be64b3412e7a83b1bd5403e2bfd49f457dd0f
Tower450 <joe.tourangeau@gmail.com>
2016-10-02 18:48:57 -0400

    res sql

--- src/dao/bookmark.rs
+++ src/dao/bookmark.rs
@@ -1,3 +1,16 @@
-fn function_name() {
-    unimplemented!()
+extern crate rusqlite;
+extern crate time;
+
+use time::Timespec;
+use rusqlite::Connection;
+
+
+#[derive(Debug)]
+struct bookmark {
+    id: i32,
+    name: String,
+    time_created: Timespec,
+    url: String,
+    stamp: Timespec,
+    revNo: i32
 }
