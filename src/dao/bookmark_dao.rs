e300e78d205cefe36857af51b1884145d11adc0b
Tower450 <joe.tourangeau@gmail.com>
2016-10-15 14:49:11 -0400

    .

--- src/dao/bookmark_dao.rs
+++ src/dao/bookmark_dao.rs
@@ -2,7 +2,9 @@ extern crate rusqlite;
 use self::rusqlite::Connection;
 
 
-pub fn create_connection() -> bool {
+
+pub fn create_connection() -> bool{
+
     Connection::open_in_memory().unwrap();
     return false;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate bookmarkt;
 use bookmarkt::dao::bookmark_dao::*;
 use bookmarkt::logic::services::*;
 
+
 fn main() {
     println!("{}", hello_from_dao());
     println!("{}", hello_from_logic());
