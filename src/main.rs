17ffde3ca3bf6131aaa64af88a08f8cbe7e888aa
vlnk <dftvlnk@gmail.com>
2016-10-15 14:45:45 -0400

    test loader

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate bookmarkt;
 
 use bookmarkt::dao::bookmark_dao::*;
+use bookmarkt::dao::query_parser::*;
 use bookmarkt::logic::services::*;
 
 fn main() {
     println!("{}", hello_from_dao());
     println!("{}", hello_from_logic());
+
+    println!("{}", load_sql_file("res/sql/bookmark/read.sql"));
 }
