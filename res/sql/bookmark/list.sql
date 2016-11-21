b0e05209eb1d293bae0b82add35323ae64bc695a
vlnk <vlnk@mail.com>
2016-11-20 19:11:13 -0500

    fix list.sql

--- res/sql/bookmark/list.sql
+++ res/sql/bookmark/list.sql
@@ -1,14 +1,4 @@
 SELECT
-<<<<<<< HEAD
-	b.id,
-	b.name,
-    b.url,
-	b.rev_no
-FROM  bookmark b
-WHERE
-	( {{ url }} IS NULL OR b.url = {{ url }}) AND
-	( {{ name }} IS NULL OR b.name = {{ name }})
-=======
     b.id,
     b.name,
     b.url,
@@ -17,5 +7,4 @@ FROM  bookmark b
 WHERE
     ( {{ url }} IS NULL OR b.url = {{ url }}) AND
     ( {{ name }} IS NULL OR b.name = {{ name }})
->>>>>>> criteria
 ;
