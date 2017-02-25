56d846a28d2ae3c345dca81e7b9645f63d2b6180
vlnk <vlnk@protonmail.ch>
2017-02-25 02:39:31 -0500

    node_map NEEDS to bo btreemap

--- src/common/node.rs
+++ src/common/node.rs
@@ -6,6 +6,7 @@ use common::entity::FromEntity;
 use common::entity::Entity;
 
 use std::collections::HashMap;
+use std::collections::BTreeMap;
 use common::tree_id::*;
 
 #[derive(Debug, Clone)]
@@ -48,7 +49,7 @@ impl FromEntity for Node {
     }
 
     fn from_entities(entities: Vec<Entity>) -> Vec<Self> {
-        let mut node_map: HashMap<String, Node> = HashMap::new();
+        let mut node_map: BTreeMap<String, Node> = BTreeMap::new();
         let mut node_dir: HashMap<i32, TreeId> = HashMap::new();
         let mut lower = usize::max_value();
         let mut pre_links: Vec<Entity> = Vec::new();
@@ -84,18 +85,19 @@ impl FromEntity for Node {
             push_node(&node_dir, &mut roots, lower, node);
         }
 
+        println!("{:?}\n", roots);
         roots
     }
 }
 
-fn push_node<'a>(dir: &HashMap<i32, TreeId>, roots: &mut Vec<Node>, search_level: usize, node: Node) {
+fn push_node(dir: &HashMap<i32, TreeId>, roots: &mut Vec<Node>, search_level: usize, node: Node) {
     let path = match dir.get(&node.id) {
         Some(tree_id) => tree_id,
         None => panic!("{:?}", node)
     };
 
     if level(path) == search_level {
-        roots.push(node.clone());
+        roots.push(node);
     } else {
         for root in roots {
             let parent_id = match key(path, search_level) {
@@ -108,8 +110,9 @@ fn push_node<'a>(dir: &HashMap<i32, TreeId>, roots: &mut Vec<Node>, search_level
                 None => panic!("{:?}", root)
             };
 
+            println!("{:?} - {:?}\n", parent_id, root_key.id());
             if parent_id == root_key.id() {
-                push_node(dir, &mut root.nodes, search_level + 1, node.clone());
+                push_node(dir, &mut root.nodes, search_level + 1, node);
                 break;
             }
         }
