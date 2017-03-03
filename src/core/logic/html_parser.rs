5535124bd231f34a5787539f6f45a6de4fa30e90
Tower450 <joe.tourangeau@gmail.com>
2017-03-02 23:16:11 -0500

    Parser/import/loadfile

--- src/core/logic/html_parser.rs
+++ src/core/logic/html_parser.rs
@@ -1,6 +1,31 @@
-use common::Entity;
+use common::utils::load_file;
+use common::entity;
+use std::fs::File;
 
-pub fn parse_html(html: String) -> Result<Vec<Entity>, String> {
-    // PARSING CODE
-    Err(String::from("unimplemented"))
+#[derive(Debug, Clone)]
+pub struct Parser{
+    bookmark_file_path: String
+}
+
+impl Parser {
+    pub fn new(bookmark_file_path: String) -> Parser{
+        Parser {
+            bookmark_file_path: bookmark_file_path
+        }
+    }
+
+    pub fn import(&mut self){
+        let filepath = &self.bookmark_file_path;
+        let f = load_file(&filepath);
+
+
+
+    }
+
+    /*
+    pub fn export() -> Result<File,Err>{
+
+    } */
+
+
 }
