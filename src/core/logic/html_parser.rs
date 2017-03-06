a49f876c255636fa3b99620caf5984a1ed15397c
Tower450 <joe.tourangeau@gmail.com>
2017-03-06 11:32:24 -0500

    test serde_json

--- src/core/logic/html_parser.rs
+++ src/core/logic/html_parser.rs
@@ -1,6 +1,13 @@
+extern crate serde_json;
+use self::serde_json::*;
 use common::utils::load_file;
-use common::entity;
+use std::str;
 use std::fs::File;
+use std::io::BufReader;
+
+
+
+
 
 #[derive(Debug, Clone)]
 pub struct Parser{
@@ -15,10 +22,26 @@ impl Parser {
     }
 
     pub fn import(&mut self){
+
         let file_path = &self.bookmark_file_path;
         let f = load_file(&file_path);
+        let s_temp = f.unwrap();
+        let file_content: &str = &s_temp[..];
 
-        println!("{:?}", f);
+        //print!("{:?}", file_content);
+
+        let json: Value = serde_json::from_str(file_content).unwrap_or_else(|e| {
+            panic!("Failed to parse json; error is {}", e);
+        });
+
+        let links = json.as_object()
+            .and_then(|object| object.get("_links"))
+            .and_then(|links| links.as_object())
+            .unwrap_or_else(|| {
+                panic!("Failed to get '_links' value from json");
+        });
+
+        //println!("{:?}", links);
 
         //let mut html_buffer = String::new();
         //try!(f.read_to_string(&mut html_buffer));
