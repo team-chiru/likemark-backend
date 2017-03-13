4516ed39ee6c067b7d4376aa5afbf3583be260c6
vlnk <vlnk@protonmail.ch>
2017-03-12 20:27:09 -0400

    debut impl json api

--- src/core/server.rs
+++ src/core/server.rs
@@ -1,8 +1,33 @@
 use iron::prelude::*;
-use iron::status;
+use iron::{ status };
+
+use iron::headers::{ AccessControlAllowOrigin, AccessControlAllowHeaders, ContentType };
+use iron::mime::{ Mime, TopLevel, SubLevel };
+use unicase::UniCase;
 
 pub fn serve() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
+
+    Iron::new(|req: &mut Request| {
+        println!("{:?}", req);
+
+        let mut resp = Response::with((status::Ok, String::from("{ \"data\": \"Hello\" }\n")));
+
+        resp.headers.set(AccessControlAllowOrigin::Any);
+        resp.headers.set(
+            AccessControlAllowHeaders(vec![
+                UniCase("content-type".to_owned())
+            ])
+        );
+
+        let sub_type = String::from("vnd.api+json");
+        resp.headers.set(
+            ContentType(
+                Mime(TopLevel::Application, SubLevel::Ext(sub_type), vec![])
+            )
+        );
+
+        println!("{:?}", resp);
+
+        Ok(resp)
     }).http("localhost:3000").unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate chrono;
 extern crate rusqlite;
 extern crate iron;
+extern crate unicase;
 
 #[macro_use] extern crate log;
 
