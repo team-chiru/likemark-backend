63e2f697a0e09e6ef44eac5a2b399299f17d3c7f
Tower450 <joe.tourangeau@gmail.com>
2017-03-13 23:58:03 -0400

    netscape remove /t /r and trim

--- src/core/external/netscape.rs
+++ src/core/external/netscape.rs
@@ -1,11 +1,29 @@
+extern crate regex;
+
 use common::node::Node;
 use core::external::converter::Converter;
+use self::regex::Regex;
 
 pub struct Netscape {}
+
+impl Netscape {
+    pub fn sanitize(bookmark_string: String) -> String {
+
+        let mut sanitized = bookmark_string;
+        sanitized = sanitized.replace("\t"," ");
+        sanitized = sanitized.replace("\r"," ");
+        /*let re1 = Regex::new(r"@<!--.*-->@mis").unwrap();
+        sanitized = re1.replace_all(&sanitized, "");*/
+        sanitized.trim();
+
+        sanitized
+    }
+}
 
 impl Converter for Netscape {
 
     fn parse(bookmark_string: String) -> Vec<Node> {
+        let bookmark_string = Netscape::sanitize(bookmark_string);
         let lines: Vec<&str> = bookmark_string.split("\n").collect();
         println!("{:?}",lines);
         vec![]
