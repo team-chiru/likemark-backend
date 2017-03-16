aa455b473b6dee37e1f857d3cfc86e01e914cee3
vlnk <vlnk@protonmail.ch>
2017-03-15 23:30:39 -0400

    merge amend visibility

--- src/core/external/mod.rs
+++ src/core/external/mod.rs
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-pub mod base;
-pub mod netscape;
-=======
 mod base;
 mod netscape;
->>>>>>> 8b20c0e9d13ff90f8c5ce835e402622171c2192c
 
 pub use self::base::Converter;
 pub type Netscape = netscape::Netscape;
