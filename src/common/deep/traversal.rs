ec660cccc14da183f1266837f68edccfc18a31e2
vlnk <vlnk@protonmail.ch>
2017-05-01 01:42:42 -0400

    WIP add incr and decr functions

--- src/common/deep/traversal.rs
+++ src/common/deep/traversal.rs
@@ -8,31 +8,64 @@ trait Traversal {
     fn down(&self) -> Option<Self> where Self: marker::Sized;
 }
 
+pub const TREE_ID_LOW_BOUND: &'static str = "0";
+pub const TREE_ID_HIGH_BOUND: &'static str = "z";
+
 fn incr(to_incr: &String) -> Option<String> {
-    unimplemented!()
+    let mut is_incr = false;
+    let rev = to_incr.chars().rev().collect::<String>();
+    let mut from_incr = String::from("");
+
+    for byte in rev.as_bytes() {
+        let old = match String::from_utf8( vec![*byte] ) {
+            Ok(s) => s,
+            Err(_) => return None
+        };
+
+        if is_incr {
+            from_incr.push_str(&old);
+        } else if old == TREE_ID_HIGH_BOUND.to_string() {
+            from_incr.push_str(TREE_ID_LOW_BOUND);
+        } else if let Ok(new) = String::from_utf8( vec![*byte + 1] ) {
+            from_incr.push_str(&new);
+            is_incr = !is_incr;
+        }
+    }
+
+    if !is_incr {
+        None
+    } else {
+        Some(from_incr.chars().rev().collect::<String>())
+    }
 }
 
 fn decr(to_decr: &String) -> Option<String> {
-    unimplemented!()
-}
+    let mut is_decr = false;
+    let rev = to_decr.chars().rev().collect::<String>();
+    let mut from_decr = String::from("");
 
-/*
-fn to_ascii(i: &i32) -> String {
-    match *i {
-        x@0...127 => format!("{:?}", x as u8 as char),
-        _ => "".into(),
+    for byte in rev.as_bytes() {
+        let old = match String::from_utf8( vec![*byte] ) {
+            Ok(s) => s,
+            Err(_) => return None
+        };
+
+        if is_decr {
+            from_decr.push_str(&old);
+        } else if old == TREE_ID_LOW_BOUND.to_string() {
+            from_decr.push_str(TREE_ID_HIGH_BOUND);
+        } else if let Ok(new) = String::from_utf8( vec![*byte - 1] ) {
+            from_decr.push_str(&new);
+            is_decr = !is_decr;
+        }
+    }
+
+    if !is_decr {
+        None
+    } else {
+        Some(from_decr.chars().rev().collect::<String>())
     }
 }
-
-fn main() {
-    for x in 0x30..0x7A {
-        print!("{} ", to_ascii(&x));
-    }
-    println!("");
-    //'0' '1' '2' '3' '4' '5' '6' '7' '8' '9' ':' ';' '<' '=' '>' '?'
-}
-*/
-
 
 impl Traversal for TreePath {
     fn right(&self) -> Option<TreePath> {
