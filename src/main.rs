5455ca2ef949147c7ba4000e1ed859fee06f4fb4
vlnk <vlnk@protonmail.ch>
2017-07-23 21:18:15 -0400

    set up cleaned main

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ extern crate bookmarkt;
 extern crate env_logger;
 extern crate rusqlite;
 
-use bookmarkt::common::utils::load_file;
-use bookmarkt::core::external::{ Converter, Netscape };
-//use bookmarkt::core::logic::html_parser::Parser;
+use bookmarkt::core::server;
 
 fn main() {
-    let bookmark_file_path = String::from("tests/core/external/input/netscape_nested.html");
-    let f = load_file(&bookmark_file_path);
-    let bookmark_string = f.unwrap();
-    Netscape::parse(bookmark_string);
+    server::serve();
+    println!("Server started on localhost:3000");
 }
